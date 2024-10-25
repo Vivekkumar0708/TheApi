ad969e2f0bd269b93368c814fede2a4b38ff7d76
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-10-25 23:16:46 +0530

    Delete TheApi/errors.py

--- TheApi/errors.py
+++
@@ -1,8 +0,0 @@
-class InvalidAmountError(Exception):
-    def __init__(self, amount):
-        super().__init__(
-            f"Invalid amount of {method} requested: jokes. Maximum allowed is 10 Minimum allowed is 1."
-        )
-class RequestError(Exception):
-    def __init__(self, err):
-        super().__init__(err)
