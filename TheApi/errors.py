0743da4a7b4cb9f19cc764e63e1f01d1f4a91422
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-25 22:43:47 +0530

    Update errors.py

--- TheApi/errors.py
+++ TheApi/errors.py
@@ -3,3 +3,6 @@ class InvalidAmountError(Exception):
         super().__init__(
             f"Invalid amount of {method} requested: jokes. Maximum allowed is 10 Minimum allowed is 1."
         )
+class RequestError(Exception):
+    def __init__(self, err):
+        super().__init__(err)
